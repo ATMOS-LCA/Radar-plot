597578b82ae7fd69c0aca81d78b0ce7a759c0dc1
Adriano Rodrigues Alves <53920233+Adrialves@users.noreply.github.com>
2024-12-12 20:57:44 -0400

    Pequenas alterações no código

--- plot_radar.py
+++ plot_radar.py
@@ -10,42 +10,57 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import pyart
 
-#Acessa a pasta data e cria uma lista com os arquivos que estão lá
-datalist = os.listdir('./data')
+from datetime import datetime
 
 #A partir da lista datalist, cria uma sub-lista apenas com os dados desejados
-typeData = input(f'Informe o tipo de dado: ')
+def typeFilter(datalist):
+    typeData = input(f'Informe o tipo de dado: ')
 
-specificData = []
-for data in datalist:
-    indice_dot = data.find('.')
-    if typeData == data[16:indice_dot]:
-        specificData.append(data)
+    filteredData = []
+    for data in datalist:
+        indice_dot = data.find('.')
+        if typeData == data[16:indice_dot]:
+            filteredData.append(data)
 
-#Tranforma da data de dd/mm/yyyy para yyyymmdd
-d1, m1, y1 = map(str, input(f'Informe uma data inicial: ').split('/'))
-d2, m2, y2 = map(str, input(f'Informe uma data final: ').split('/'))
+    return filteredData
+    #print(filteredData)
 
-filteredData = []
-for data in specificData:
-    if data[0:4] >= y1 and data[0:4] <= y2:
-        if data[4:6] >= m1 and data[4:6] <= m2:
-            if data[6:8] >= d1 and data[6:8] <= d2:
-                filteredData.append(data)
+def timeFilter(datalist):
+    data_inicial_str = input(f'Informe uma data inicial (dd/mm/yyyy): ')
+    data_final_str = input(f'Informe uma data final (dd/mm/yyyy): ')
+    
+    #Converte as datas de entrada para o formato datetime a partir do formato dd/mm/yyyy
+    data_inicial = datetime.strptime(data_inicial_str, "%d/%m/%Y")
+    data_final = datetime.strptime(data_final_str, "%d/%m/%Y")
 
-fpath = './data/' + datalist[0]
-frames = []
-data = wrl.io.hdf.read_generic_hdf5(os.path.join(fpath))
-for i in range(1, 14):
-    fig = plt.figure(figsize=(10,10))
-    da = wrl.georef.create_xarray_dataarray(data["dataset"+str(i)+"/data1/data"]["data"]).wrl.georef.georeference()
-    im = da.wrl.vis.plot(fig=fig, crs="cg")
+    filteredData = []
+    for data in datalist:
+        #Converte a data do arquivo para o formato datetime a partir do formato yyyymmdd
+        data_arquivo = datetime.strptime(data[0:8], "%Y%m%d")
+        if data_inicial <= data_arquivo <= data_final:
+            filteredData.append(data)
 
-    aux = "dataset"+str(i)
-    frame_path = f'./frames/{aux}.png'
-    plt.savefig(frame_path, dpi=100)
-    frames.append(imageio.imread(frame_path))
-    plt.close()
+    return filteredData
+    #print(filteredData)
 
-gif_path = './gifs/dBZ.gif'
-imageio.mimsave(gif_path, frames, fps=1)
+#Acessa a pasta data e cria uma lista com os arquivos que estão lá
+datalist = os.listdir('./data')
+arquivos = timeFilter(typeFilter(datalist))
+for arquivo in arquivos:
+    fpath = './data/' + arquivo
+    frames = []
+    data = wrl.io.hdf.read_generic_hdf5(os.path.join(fpath))
+    
+    for i in range(1, 14):
+        fig = plt.figure(figsize=(10,10))
+        da = wrl.georef.create_xarray_dataarray(data["dataset"+str(i)+"/data1/data"]["data"]).wrl.georef.georeference()
+        im = da.wrl.vis.plot(fig=fig, crs="cg")
+
+        aux = "dataset"+str(i)
+        frame_path = f'./frames/{aux}.png'
+        plt.savefig(frame_path, dpi=100)
+        frames.append(imageio.imread(frame_path))
+        plt.close()
+
+    gif_path = './gifs/'+arquivo+'.gif'
+    imageio.mimsave(gif_path, frames, fps=1, loop=0)
